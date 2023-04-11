f086e6b4b663cb03cca8a6d2e93538d2c48c22f8
Elli Cabuhat <johnelli.cabuhat@gmail.com>
2023-04-11 22:45:46 +0800

    url change

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -1,15 +1,15 @@
 import * as actions from "./actionType";
 import axios from "axios";
 
-let BASE_URL_ENV = "";
+let BASE_URL_ENV = "https://elli-capstone-backend-uat.azurewebsites.net";
 
-if (process.env.NODE_ENV === "development") {
-  BASE_URL_ENV = "https://localhost:7073";
-} else if (process.env.NODE_ENV === "uat") {
-  BASE_URL_ENV = "https://elli-capstone-backend-uat.azurewebsites.net";
-} else if (process.env.NODE_ENV === "prod") {
-  BASE_URL_ENV = "https://elli-capstone-backend-prod.azurewebsites.net";
-}
+// if (process.env.NODE_ENV === "development") {
+//   BASE_URL_ENV = "https://localhost:7073";
+// } else if (process.env.NODE_ENV === "uat") {
+//   BASE_URL_ENV = "https://elli-capstone-backend-uat.azurewebsites.net";
+// } else if (process.env.NODE_ENV === "prod") {
+//   BASE_URL_ENV = "https://elli-capstone-backend-prod.azurewebsites.net";
+// }
 
 export const onGetEmp = () => {
   return async (dispatch) => {
